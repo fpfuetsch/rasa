a70b0e32e21a05eba4b9c46276defc2a19b797ca
fpfuetsch <54020707+fpfuetsch@users.noreply.github.com>
2020-10-15 10:51:33 +0200

    Fix slack connector - metadata extraction

--- rasa/core/channels/slack.py
+++ rasa/core/channels/slack.py
@@ -324,21 +324,37 @@ class SlackInput(InputChannel):
 
     def get_metadata(self, request: Request) -> Dict[Text, Any]:
         """Extracts the metadata from a slack API event (https://api.slack.com/types/event).
-
         Args:
             request: A `Request` object that contains a slack API event in the body.
-
         Returns:
             Metadata extracted from the sent event payload. This includes the output channel for the response,
             and users that have installed the bot.
         """
-        slack_event = request.json
-        event = slack_event.get("event", {})
+        content_type = request.headers.get("content-type")
 
-        return {
-            "out_channel": event.get("channel"),
-            "users": slack_event.get("authed_users"),
-        }
+        # Slack API sends either a JSON-encoded or a URL-encoded body depending on the content
+        if content_type == "application/json":
+            # if JSON-encoded message is received
+            slack_event = request.json
+            event = slack_event.get("event", {})
+
+            return {
+                "out_channel": event.get("channel"),
+                "users": slack_event.get("authed_users"),
+            }
+
+        if content_type == "application/x-www-form-urlencoded":
+            # if URL-encoded message is received
+            output = request.form
+            payload = json.loads(output["payload"][0])
+            message = payload.get("message", {})
+            thread_id = message.get("thread_ts", message.get("ts"))
+            return {
+                "out_channel": payload.get("channel", {}).get("id"),
+                "users": payload.get("user", {}).get("id"),
+            }
+
+        return {}
 
     def blueprint(
             self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
